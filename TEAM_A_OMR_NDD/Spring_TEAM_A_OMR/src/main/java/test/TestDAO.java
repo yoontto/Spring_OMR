0f8325773910a604ad48575b51b864efabd4cb82
NDD <gkvhaaa@gmail.com>
2021-08-31 19:33:33 +0900

    08.31. 오후 7:33

---
+++ TEAM_A_OMR_NDD/Spring_TEAM_A_OMR/src/main/java/test/TestDAO.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TestDAO implements TestService{
+	@Autowired @Qualifier("hanul") private SqlSession sql;
+
+	
+	@Override
+	public List<TestVO> listQuestion(String subjectName) {
+		return sql.selectList("listQuestion", subjectName);
+	}
+
+	@Override
+	public int insertResult(ResultVO vo, String subjectName) {
+		// TODO Auto-generated method stub
+		return sql.insert("saveResult", subjectName);
+	}
+
+	@Override
+	public List<ResultVO> getResult(String subjectName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int modifyQuestion(TestVO vo, String subjectName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertQuestion(TestVO vo, String subjectName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
